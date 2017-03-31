62c8fd32f30a0dfc63d4c670423d74e4be0936d4
MCHellspawn <joesherman19792gmail.com>
2017-03-30 20:22:53 -0400

    3/30/17 - Block loaded!!

--- src/main/java/mchellspawn/compressedfurnace/init/ModBlocks.java
+++ src/main/java/mchellspawn/compressedfurnace/init/ModBlocks.java
@@ -4,15 +4,18 @@ import mchellspawn.compressedfurnace.blocks.BlockCompressedFurnace;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
 	
 	public static Block compressedfurnace;
+	public static ItemBlock itmblkcompressedfurnace;
 
 	public static void init() {
 		compressedfurnace = new BlockCompressedFurnace();
+		itmblkcompressedfurnace = new ItemBlock(compressedfurnace);
 		mchellspawn.compressedfurnace.compressedfurnace.logger.info(compressedfurnace.getUnlocalizedName());
 		mchellspawn.compressedfurnace.compressedfurnace.logger.info(compressedfurnace.getRegistryName());
 
@@ -20,6 +23,7 @@ public class ModBlocks {
 	
 	public static void register() {
 		GameRegistry.register(compressedfurnace);
+		GameRegistry.register(itmblkcompressedfurnace, compressedfurnace.getRegistryName());
 	}
 	
 	public static void registerRenders() {
@@ -31,5 +35,6 @@ public class ModBlocks {
 		
 		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(block.getRegistryName(),"inventory"));
 		mchellspawn.compressedfurnace.compressedfurnace.logger.info("Render for block " + block.getUnlocalizedName() + " registered.");
+		
 	}
 }
